9ff76aabbefa7e2793b06ec1464be95833f19a42
sushanthavvaru <sushantha.a@gmail.com>
2017-09-12 01:51:24 -0700

    user info added in log

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const hbs = require("hbs");
 const fs = require('fs');
+const os = require('os')
+const user = os.userInfo();
 
 const port = process.env.PORT || 3000;
 
@@ -11,7 +13,8 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) =>{
     var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
+
+    var log = `${now}: ${req.method} ${req.url} by ${user.username}`;
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) =>{
         if(err){
